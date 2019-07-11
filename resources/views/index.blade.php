0d7bb13dee7ce756784c46d1e09c74d73dcd7bbb
Kistlak Rajapaksha <kistlakall@gmail.com>
2019-07-11 09:05:57 +0530

    database migration and index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <style>
+    .UpdateInputs
+    {
+        background-color: white !important;
+        border: 0px solid white;
+    }
+    .UpdateInputsTot
+    {
+        background-color: white !important;
+        border: 0px solid white;
+    }
+    </style>
+
+</head>
+
+<body>
+
+
+
+</body>
+</html>
